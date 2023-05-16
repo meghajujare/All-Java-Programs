6b6b8e1a68fc9de3d75d5c53cb8aeaa124728b0c
Megha A J <120260066+meghajujare@users.noreply.github.com>
2023-05-16 18:50:19 +0530

    Update and rename Swap.java to Swap_Numbers.java

--- Swap_Numbers.java
+++ Swap_Numbers.java
@@ -1,6 +1,6 @@
  import java.util.*;
 
-public class Swap {
+public class Swap_Numbers {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int a, b, temp;
